42b51048b64c127a003970b8b069cadbfbd69093
Mosesmpho <mosesmpho16@gmail.com>
2022-06-09 11:23:28 +0200

    Add project files.

---
+++ ADDING INTERESTS TO THE AMOUNT.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double balance = 1000;
+	double totalbalance;
+
+	totalbalance = balance * 0.045;
+
+	cout << totalbalance;
+
+	return 0;
+}
